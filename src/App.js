0034ea07efddd26d285572818756df22dce92fbe
Hassan Mian <mian@willandskill.se>
2021-01-13 15:31:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import ActionPoint from './components/ActionPoint';
 
 function App() {
+  const linkInternal = {getFrontendUrl: "/myFrontendURL", title: "My Frontend URL"}
+  const linkDocument = "https://www.google.se/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+  const linkExternal = "https://www.google.com"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ActionPoint 
+        title="This is a internal link title"
+        description="This is a description"
+        top={10}
+        left={10}
+        linkKind={0}
+        linkInternal={linkInternal}
+        iconKind={0}
+      />
+
+      <ActionPoint 
+        title="This is a external link title"
+        description="This is a description"
+        top={30}
+        left={20}
+        linkKind={1}
+        linkExternal={linkExternal}
+        iconKind={1}
+      />
+      
+      <ActionPoint 
+        title="This is a Modal link title"
+        description="This is a description"
+        top={50}
+        left={30}
+        linkKind={2}
+        iconKind={1}
+      />
+      
+      <ActionPoint 
+        title="This is a Document link title"
+        description="This is a description"
+        top={70}
+        left={40}
+        linkKind={3}
+        iconKind={1}
+        linkDocument={linkDocument}
+      />
+      
     </div>
   );
 }
